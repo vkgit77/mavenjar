5559eeaf64a7f5f6b6ece73991405d852e25e2a0
kishore dodda <kishore.dodda1@gmail.com>
2025-10-15 15:50:13 +0800

    commiting src

---
+++ src/main/java/com/rknsit/services/App.java
@@ -0,0 +1,19 @@
+package com.rknsit.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+	System.out.println( "Maven Project" );
+
+	System.out.println( "insert new record" );
+
+
+    }
+}
